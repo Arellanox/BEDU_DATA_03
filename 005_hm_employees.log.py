bd79e22ddc7d747d295a93d65dc71ab6ece9bd5d
Arellanox <arellanox0392@gmail.com>
2020-08-22 13:52:22 -0500

    Tarea 2

---
+++ 005_hm_employees.log.py
@@ -0,0 +1,8 @@
+# Given 10000 logs
+
+# Group log by employee
+
+# number of each category enter_for  an employee has entered
+#i.e. Employee 1002 meeting - 52, call -80
+
+# Show hot hours for enter to the facilities
